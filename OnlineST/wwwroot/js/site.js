f6dd0d92a376dd43df048946c69ce3ad8d002136
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2021-03-12 19:27:58 -0300

    .

--- OnlineST/wwwroot/js/site.js
+++ OnlineST/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function ClickInputFile() {
+
+    const spanFileName = document.getElementById("FileName");
+    const inputFile = document.getElementById("InputFile");
+
+    spanFileName.textContent = "";
+
+    inputFile.click();
+}
+
+function UploadImage() {
+
+    const spanFileName = document.getElementById("FileName");
+    const inputFile = document.getElementById("InputFile");
+
+    spanFileName.textContent = inputFile.files[0].name;
+}
